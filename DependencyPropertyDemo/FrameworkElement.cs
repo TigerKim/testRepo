4db262583b2be13e12e014acfdf9e0bfd8eed891
TigerKim <df072@nate.com>
2012-07-12 13:23:32 +0900

    DependencyProperty 176page

---
+++ DependencyPropertyDemo/FrameworkElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace DependencyPropertyDemo
+{
+    public class FrameworkElement : UIElement
+    {
+        public static readonly DependencyProperty MarginProperty;
+        public Thickness Margin
+        {
+            set { SetValue(MarginProperty, value); }
+            get { return (Thickness)GetValue(MarginProperty); }
+        }
+        static FrameworkElement()
+        {
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata(
+                new Thickness(), FrameworkPropertyMetadataOptions.AffectsMeasure);
+            MarginProperty = DependencyProperty.Register("Margin", 
+                typeof(Thickness), typeof(FrameworkElement), metadata,
+                new ValidateValueCallback(FrameworkElement.IsMarginValid));
+        }
+        private static bool IsMarginValid(object value)
+        {
+            Thickness thickness1 = (Thickness)value;
+            return thickness1.IsValid(true, false, true, false);
+        }
+
+    }
+}
