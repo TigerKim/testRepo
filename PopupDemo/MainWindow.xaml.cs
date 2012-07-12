5b522f2f4bbc2a97c1ed7aaf2b4ac3388ea5731e
TigerKim <df072@nate.com>
2012-07-12 21:42:46 +0900

    Popup 244page

---
+++ PopupDemo/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace PopupDemo
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void run_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popLink.IsOpen = true;
+        }
+
+        private void lnk_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(((Hyperlink)sender).NavigateUri.ToString());
+        }
+    }
+}
