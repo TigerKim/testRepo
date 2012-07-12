3e2d5143a13adf65d4ca36da47438f85581ef306
TigerKim <df072@nate.com>
2012-07-12 09:37:35 +0900

    ScrollViewer Demo 162page

---
+++ ScrollDemo/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScrollDemo
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LineUp_Click(object sender, RoutedEventArgs e)
+        {
+            scrollViewer1.LineUp();
+        }
+
+        private void LineDown_Click(object sender, RoutedEventArgs e)
+        {
+            scrollViewer1.LineDown();
+        }
+
+        private void PageUp_Click(object sender, RoutedEventArgs e)
+        {
+            scrollViewer1.PageUp();
+        }
+
+        private void PageDown_Click(object sender, RoutedEventArgs e)
+        {
+            scrollViewer1.PageDown();
+        }
+
+       
+    }
+}
