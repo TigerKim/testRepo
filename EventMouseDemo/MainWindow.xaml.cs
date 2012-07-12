1412da00f917bca877205e9890d2a576c021a5ab
TigerKim <df072@nate.com>
2012-07-12 16:14:05 +0900

    MouseMove 214page

---
+++ EventMouseDemo/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventMouseDemo
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point pt = e.GetPosition(this);
+            lblInfo.Content = String.Format("Your are at ({0}, {1}) in window coordinates", pt.X, pt.Y);
+        }
+    }
+}
