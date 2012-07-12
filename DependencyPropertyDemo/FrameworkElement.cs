558cf3773be64d93d3d40018c0577f9b6214ddf8
TigerKim <df072@nate.com>
2012-07-12 14:12:46 +0900

    Bubbling Event 196page

--- DependencyPropertyDemo/FrameworkElement.cs
+++ DependencyPropertyDemo/FrameworkElement.cs
@@ -8,25 +8,25 @@ namespace DependencyPropertyDemo
 {
     public class FrameworkElement : UIElement
     {
-        public static readonly DependencyProperty MarginProperty;
-        public Thickness Margin
-        {
-            set { SetValue(MarginProperty, value); }
-            get { return (Thickness)GetValue(MarginProperty); }
-        }
-        static FrameworkElement()
-        {
-            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata(
-                new Thickness(), FrameworkPropertyMetadataOptions.AffectsMeasure);
-            MarginProperty = DependencyProperty.Register("Margin", 
-                typeof(Thickness), typeof(FrameworkElement), metadata,
-                new ValidateValueCallback(FrameworkElement.IsMarginValid));
-        }
-        private static bool IsMarginValid(object value)
-        {
-            Thickness thickness1 = (Thickness)value;
-            return thickness1.IsValid(true, false, true, false);
-        }
+        //public static readonly DependencyProperty MarginProperty;
+        //public Thickness Margin
+        //{
+        //    set { SetValue(MarginProperty, value); }
+        //    get { return (Thickness)GetValue(MarginProperty); }
+        //}
+        //static FrameworkElement()
+        //{
+        //    FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata(
+        //        new Thickness(), FrameworkPropertyMetadataOptions.AffectsMeasure);
+        //    MarginProperty = DependencyProperty.Register("Margin", 
+        //        typeof(Thickness), typeof(FrameworkElement), metadata,
+        //        new ValidateValueCallback(FrameworkElement.IsMarginValid));
+        //}
+        //private static bool IsMarginValid(object value)
+        //{
+        //    Thickness thickness1 = (Thickness)value;
+        //    return thickness1. (true, false, true, false);
+        //}
 
     }
 }
