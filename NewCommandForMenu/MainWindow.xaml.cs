059f7d15511bdb7968b6783b2902750c5879e9c5
TigerKim <df072@nate.com>
2012-07-13 12:57:07 +0900

    다중명령소스 사용하기 348page

---
+++ NewCommandForMenu/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewCommandForMenu
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            CommandBinding binding = new CommandBinding(ApplicationCommands.New);
+            binding.Executed += new ExecutedRoutedEventHandler(binding_Executed); 
+            //new ExecutedRoutedEventHandler(binding_Executed);
+            this.CommandBindings.Add(binding);
+
+            InitializeComponent();
+        }
+
+        void binding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("New Command");
+        }
+    }
+}
