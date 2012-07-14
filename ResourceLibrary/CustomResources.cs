d0dfe8867e6b462de773c745736073ebf92a9377
TigerKim <df072@nate.com>
2012-07-14 18:46:27 +0900

    어셈블리사이에 리소스 공유하기 396page

---
+++ ResourceLibrary/CustomResources.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace ResourceLibrary
+{
+    public class CustomResources
+    {
+        public static ComponentResourceKey SadTileBrushKey
+        {
+            get
+            {
+                return new ComponentResourceKey(typeof(CustomResources), "SadTileBrush");
+            }
+        }
+    }
+}
