127d673cdb15155f14b591cdf2d69077d88c9994
TigerKim <df072@nate.com>
2012-07-14 20:04:09 +0900

    Event Handler Connection 408page

---
+++ StyleEventHandler/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StyleEventHandler
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void element_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((TextBlock)sender).Background = new
+                SolidColorBrush(Colors.LightGoldenrodYellow);
+        }
+        private void element_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((TextBlock)sender).Background = null;
+        }
+    }
+}
