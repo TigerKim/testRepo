4be7c0058a877314d1c24959cd0448289f95ce9e
TigerKim <df072@nate.com>
2012-07-16 01:54:12 +0900

    546 요소의 프로퍼티에 바인딩하기

---
+++ BindingDemo1ElementBinding/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingDemo1ElementBinding
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetSmall_Click(object sender, RoutedEventArgs e)
+        {
+            sliderFontSize2.Value = 10;
+        }
+
+        private void SetNormal_Click(object sender, RoutedEventArgs e)
+        {
+            sliderFontSize2.Value = 20;
+        }
+
+        private void SetLarge_Click(object sender, RoutedEventArgs e)
+        {
+            lblSampleText2.FontSize = 40; // 바인딩을 없애버린다
+        }
+
+        private void SetSmall3_Click(object sender, RoutedEventArgs e)
+        {
+            sliderFontSize3.Value = 10;
+        }
+
+        private void SetNormal3_Click(object sender, RoutedEventArgs e)
+        {
+            sliderFontSize3.Value = 20;
+        }
+
+        private void SetLarge3_Click(object sender, RoutedEventArgs e)
+        {
+            lblSampleText3.FontSize = 40; // 바인딩을 없애버린다
+        }
+    }
+}
